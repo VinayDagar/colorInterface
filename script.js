212aeb04abc0552163465d14420086059f491286
vinay <vinay@dagar.com>
2018-12-29 18:09:53 +0530

    Added copied text on colors

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 let copy = function (e) {
     text = e.toElement.innerText;
+    console.log(text)
     input = document.querySelector('.hide');
+
     input.setAttribute('value', text);
     input.select()
-    document.execCommand("Copy")
+    document.execCommand("Copy");
+
+    setTimeout(( )=>{
+        e.toElement.innerText = "Copied"
+    },100);
+    setTimeout(()=>{
+        e.toElement.innerText = text
+    },1000)
+    
 }
 
 window.addEventListener('scroll', (value) => {
